3adccb310d51f364a684fbe89573c85a737e2d57
akoarguel <manel.arguelles@gmail.com>
2025-12-04 12:17:15 +0100

    04/12 clase

--- Prácticas/UNIDAD 2/Ejemplos clase - spyder/ud2.py
+++ Prácticas/UNIDAD 2/Ejemplos clase - spyder/ud2.py
@@ -92,3 +92,197 @@ axs[1,0].scatter(x, y, color='red')
 axs[1,0].set_title('Dispersión')
 axs[1,1].hist(y, bins=4, color='purple')
 axs[1,1].set_title('Histograma')
+
+#%% pandas
+import pandas as pd
+ruta = r"C:\Users\Mañana\Documents\akoarguel\Sistemas-de-Aprendizaje-Automatico\Prácticas\UNIDAD 2\Ejemplos clase - spyder\unsdg_2002_2021.csv"
+df = pd.read_csv(ruta, sep=',')
+# print(df.head())
+# print(df.info())
+# print(df.describe())
+# print(df.columns.values)
+# print(df.index)
+print("Ordenando valores de las fechas")
+print(df.sort_values("dt_year", ascending=True))
+print("usando iloc[] para la primera fila")
+print(df.iloc[1])
+print("usando loc[] para dt_year")
+# df_2002= df.loc[df["dt_year"]==2002]
+# print(df_2002)
+print(df.drop_duplicates())
+print(df.drop_duplicates(subset='dt_year'))
+print("Corrigiendo NAN")
+print(df.dropna(how='any'))#elimina si hay algun NAN
+print(df.dropna(how='all'))#elimina todos los NAN
+print(df.fillna(0))#remplaza los nan por 0.0
+ 
+ 
+#%% SimpleImputer
+ 
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer 
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+#cargar el dataset
+ 
+ruta = r"C:\Users\Mañana\Documents\akoarguel\Sistemas-de-Aprendizaje-Automatico\Prácticas\UNIDAD 2\Ejemplos clase - spyder\unsdg_2002_2021.csv"
+df = pd.read_csv(ruta, sep=',')
+
+#Seleccionar categoria y crear dataset
+dfObjetivo = df[["country","level_of_development"]]
+dfObjetivo = dfObjetivo.drop_duplicates()
+ 
+#prueba con df normal
+print(df.head())
+#prueba con df objetivo
+print(dfObjetivo.head())
+ 
+dfCaracteristicas = df.drop(columns=["dt_year","dt_date","region"])
+#agrupar por pais y calcular la media
+dfCaracteristicas = dfCaracteristicas.groupby("country").mean(numeric_only=True)
+print (dfCaracteristicas)
+ 
+imputador = SimpleImputer(missing_values=np.nan, strategy="mean")
+dfImputado = imputador.fit_transform(dfCaracteristicas)
+#Convertirlo a dataframe
+ 
+dfImputado = pd.DataFrame(dfImputado,index=dfCaracteristicas.index,
+                          columns=dfCaracteristicas.columns)
+ 
+#Prueba con dfImputado
+print(dfImputado.head())
+ 
+#estandalizar los datos
+escalador = StandardScaler()
+dfEstandarizado = escalador.fit_transform(dfImputado)
+ 
+#convertirlo a dataframe
+X = pd.DataFrame(dfEstandarizado,index=dfCaracteristicas.index,
+                               columns=dfCaracteristicas.columns)
+ 
+ 
+#Preparamos la variable objetivo Y
+Y = dfObjetivo.set_index("country")
+ 
+#Caracteristicas estandarizadas (x)
+print(X.head())
+ 
+#Nivel de desarrollo (y)
+print(Y.head())
+ 
+#dfObjetivo sera como Y pero  con otro index
+print(dfObjetivo.head())
+ 
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X,
+    Y,
+    test_size= 0.30,
+    random_state=42)
+ 
+print("\n------Tamaños de los conjuntos")
+print("\n------X_train", X_train.shape)
+print("\n------X_test", X_test.shape)
+print("\n------Y_train", Y_train.shape)
+print("\n------Y_test", Y_test.shape)
+# print("\n------Y_train", Y_train)
+# print("\n------X_train", X_train)
+
+#%% Practica VIII - WALMART
+
+import pandas as pd
+import random
+
+dfDatos = pd.DataFrame()
+
+def cargarDatos():
+    dfDatos = pd.read_csv( r"C:\Users\Mañana\Documents\akoarguel\Sistemas-de-Aprendizaje-Automatico\Prácticas\UNIDAD 2\Ejemplos clase - spyder\Walmart_Sales.csv")
+    
+    valoresPosiblesWeeklyRains = ["Ninguna", "Pocas", "Medias", "Muchas"]
+    weekly_Rains = []
+    for i in range(dfDatos.shape[0]):
+        weekly_Rains.append(valoresPosiblesWeeklyRains[random.randint(0, 3)])
+        
+    dfDatos["Weekly_Rains"] = weekly_Rains
+    
+    valoresPosiblesWeeklyDiscounts = ["Carnes", "Pescados", "Restos"]
+    weekly_Discounts = []
+    for i in range(dfDatos.shape[0]):
+        weekly_Discounts.append(valoresPosiblesWeeklyDiscounts[random.randint(0, 2)])
+        
+    dfDatos["Weekly_Discounts"] = weekly_Discounts
+    
+    dfDatos = dfDatos[dfDatos.Store == 1]
+    return dfDatos
+
+dfDatos = cargarDatos()
+print("\n\nTipo de datos: ")
+print(type(dfDatos))
+print("\n\nDataframe dfDatos-------------------")
+print(dfDatos)
+print("\n\nVer si hay algun null---------------")
+print(dfDatos.isnull())
+print("\n\nLa suma de cuantos null hay---------")
+print(dfDatos.isnull().sum())
+
+#%% Ejercicio 1 walmart
+"""
+1. Carga walmart.csv, crea dos columnas categóricas (no numéricas) nuevas
+distintas, por ejemplo: Customer_Flow: ["Bajo", "Medio", "Alto", "Muy_Alto"].
+Una columna de dos valores y otra de cuatro
+"""
+
+
+import pandas as pd
+import random
+
+dfDatos = pd.DataFrame()
+
+dfDatos = pd.read_csv( r"C:\Users\Mañana\Documents\akoarguel\Sistemas-de-Aprendizaje-Automatico\Prácticas\UNIDAD 2\Ejemplos clase - spyder\Walmart_Sales.csv")
+    
+valoresPosiblesCustomerFlow = ["Bajo", "Medio", "Alto", "Muy_Alto"]
+valoresPosiblesCustomerFlow2 = ["NO", "SI"]
+  
+# Ejercicio 2 walmart
+
+"""
+Define una función llamada insertarColumnas que use bucles para insertar las
+columnas que has creado en el dataframe y que utilice solo los datos de la 
+‘Store’ número 2. Imprime.el datraframe resultante
+"""
+
+def insertarColumnas(vpcf1, vpcf2, dfDatos):
+    customer_flow = []
+    for i in range(dfDatos.shape[0]):
+        customer_flow.append(vpcf1[random.randint(0, 3)])
+    dfDatos["Ventas"] = customer_flow
+    
+    customer_flow2 = []
+    for i in range(dfDatos.shape[0]):
+        customer_flow2.append(vpcf2[random.randint(0, 1)])
+    dfDatos["Partidad al LOL"] = customer_flow2
+    
+    dfDatos = dfDatos[dfDatos.Store == 2]
+    return dfDatos
+
+dfDatos = insertarColumnas(valoresPosiblesCustomerFlow, valoresPosiblesCustomerFlow2, dfDatos)
+print(dfDatos.shape[0])
+print("\n\nTipo de datos:")
+print(type(dfDatos))
+print("\n\nDataframe dfDatos----------:\n")
+print(dfDatos)
+
+
+# Ejercicio 3 walmart
+"""
+Verifica que tu dataFrame no tiene ningún dato nulo mostrándo la suma de los
+null
+"""
+
+print("\n\nVer si hay algun null----------:\n")
+print(dfDatos.isnull())
+print("\n\nLa suma de cuantos null hay----------:\n")
+print(dfDatos.isnull().sum())
+
+
+
--- Prácticas/UNIDAD 2/Ejercicios propuestos - spyder/ejp2.py
+++ Prácticas/UNIDAD 2/Ejercicios propuestos - spyder/ejp2.py
@@ -7,6 +7,9 @@ Created on Fri Nov 14 11:05:08 2025
 
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.impute import SimpleImputer
+import pandas as pd
 
 #%% Ejercicio 15
 x = np.random.randint(0,90, (3,1))
@@ -46,3 +49,75 @@ axs[1].set_title("Grafico ejercicio 1")
 axs[1].set_xlabel("eje x")
 axs[1].set_xlim(0,90)
 axs[1].set_ylim(0,600)
+
+#%% Conjunto de datos scikit-learn
+ 
+datos_diabetes = datasets.load_diabetes()
+#datos_boston = datasets.load_boston()
+datos_iris = datasets.load_iris()
+datos_cancer = datasets.load_breast_cancer()
+datos_digits = datasets.load_digits()
+ 
+#elegimos un dataset
+datos = datos_iris
+print(datos.keys())
+print("\nNombres de las caracteristicas -----")
+print(datos.feature_names)
+print("\nNombres del objetivo -----")
+print(datos.target_names)
+print("\nNombres de frame -----")
+print(datos.frame)
+print("\nNombres de filename -----")
+print(datos.filename)
+print("\n\n\n Description -----")
+print(datos.DESCR)
+ 
+print("\nDescripcion datos digits -----")
+print(datos_digits.keys())
+print(datos_digits.DESCR)
+ 
+ 
+df_x = pd.DataFrame(datos.data, columns=datos.feature_names)
+#index = datos.index
+print("\nDataframe de caracteristicas (X)")
+print(df_x.head())
+
+#%% Ejercicio 1
+'''1 Se pide cargar el conjunto de datos breast_cancer de Scikit-learn, 
+convertirlo en un DataFrame de pandas usando los nombres de las 
+características como columnas y mostrar por pantalla las cinco 
+primeras filas del DataFrame resultante.'''
+
+datos_cancer = datasets.load_breast_cancer()
+
+df =pd.DataFrame(datos_cancer.data, columns=datos_cancer.feature_names)
+
+print("Cinco primeras filas")
+print(df.head())
+
+
+#%% Ejercicio 2
+'''A partir del DataFrame anterior, se solicita aplicar un objeto 
+SimpleImputer de Scikit-learn configurado con la estrategia "most_frequent"
+ para sustituir todos los valores 0.0 del conjunto de datos por el valor 
+ más frecuente de cada columna, y guardarlo en un nuevo DataFrame llamado 
+ dfImputado. Mostrarlo por pantalla.'''
+
+imputer = SimpleImputer(missing_values=0.0, strategy='most_frequent')
+datos_imputados = imputer.fit_transform(df) 
+
+dfImputado=pd.DataFrame(data=datos_imputados, columns=df.columns)
+print("Df por pantalla")
+print(dfImputado.head())
+ 
+#%% Ejercicio 3
+''' Crear un DataFrame llamado dfCancerModificado usando el método .copy() 
+en el dfImputado, que elimine la columna "worst symmetry" y ordene el 
+DataFrame por la columna "mean texture" en orden ascendente.'''
+
+dfCancerModificado = dfImputado.copy()
+dfCancerModificado = dfCancerModificado.drop("worst symmetry", axis=1)
+dfCancerModificado = dfCancerModificado.sort_values(by="mean texture", ascending=True)
+
+print("Df Cancer modificado")
+print(dfCancerModificado.head())   
